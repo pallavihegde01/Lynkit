664dcef472fa9e42bb43eb2bdd35619f350c8d6c
Pallavi Hegde <pallavighegde16@gmail.com>
2025-07-17 23:07:39 +0530

    setting up

---
+++ backend/src/server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+import authRoutes from "./routes/auth.route.js";
+import { connectDB } from "./lib/db.js";
+
+const app = express();
+const PORT=process.env.PORT
+
+app.use("/api/auth", authRoutes)
+
+app.listen(PORT,()=>{
+    console.log(`server is running on port ${PORT}`);
+    connectDB();
+});
