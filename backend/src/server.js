c25a14ff1429f952cd51aa1cc05b613ab643fb30
Pallavi Hegde <pallavighegde16@gmail.com>
2025-07-22 18:13:20 +0530

    added onboard

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,6 @@
 import express from "express";
-import dotenv from "dotenv";
-
-dotenv.config();
+import dotenv from "dotenv/config";
+import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.route.js";
 import { connectDB } from "./lib/db.js";
@@ -10,7 +9,10 @@ const app = express();
 const PORT=process.env.PORT
 
 app.use(express.json());
-app.use("/api/auth", authRoutes)
+app.use(cookieParser());
+
+app.use("/api/auth", authRoutes);
+
 
 app.listen(PORT,()=>{
     console.log(`server is running on port ${PORT}`);
