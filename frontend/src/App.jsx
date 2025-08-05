26be82c1d1064ff12b2feac96807058d9f890e12
Pallavi Hegde <pallavighegde16@gmail.com>
2025-08-05 15:12:48 +0530

    chat page added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -31,7 +31,7 @@ const App = () => {
         <Route path='/login' element={!isAuthenticated ? <LoginPage/> : <Navigate to={isOnboarded ? "/" : "/onboarding"}/>}/>
         <Route path='/notifications' element={isAuthenticated && isOnboarded ?(<Layout showSidebar={true}><NotificationsPage/></Layout>) : (<Navigate to={!isAuthenticated ? "/login" : "/onboarding"} />)}/>
         <Route path='/call' element={isAuthenticated ?<CallPage/> : <Navigate to="/login"/>}/>
-        <Route path='/chat' element={isAuthenticated ?<ChatPage/> : <Navigate to="/login"/>}/>
+        <Route path='/chat/:id' element={isAuthenticated && isOnboarded ? (<Layout showSidebar={false}><ChatPage /></Layout>) : (<Navigate to={!isAuthenticated ? "/login" : "/onboarding"} />) }/>
         <Route path='/onboarding' element={isAuthenticated ? (!isOnboarded ? (<OnboardingPage/>) : (<Navigate to="/"/>)) : (<Navigate to="/login"/>)}/>
       </Routes>
       <Toaster/>
